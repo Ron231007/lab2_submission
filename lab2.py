4e842e38f5b4e6c966f364c0f3568609136d77d5
Rondell <rondelllit16@gmail.com>
2025-05-07 00:03:43 +0800

    added the function to find median

--- lab2.py
+++ lab2.py
@@ -1,3 +1,5 @@
+import statistics
+
 def DisplayMenu():
     print("Enter some numbers separated by commas (e.g. 5, 67,37)")
 
@@ -19,5 +21,19 @@ def average_temperature(temp):
         sum += x
     
     return (sum/len(temp))
+
+def calc_median_temperature(temp):
+   return statistics.median(temp)
+   
+   
+    # temp.sort()
+    # iseven = len(temp)%2 ==0
+    # middle = len(temp) //2
+    
+    # if(iseven):
+    #    return round((temp[middle] + temp[middle +1])/2,2)
+    
+    # else:
+    #     return temp[middle]
 
 
