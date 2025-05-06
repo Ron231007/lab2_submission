7b607c07f7c8fc64a70bde36cbc55b7d5bd9b6a1
Rondell <rondelllit16@gmail.com>
2025-05-06 23:53:19 +0800

    Lab2_v1.1

--- lab2.py
+++ lab2.py
@@ -1,1 +1,23 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def DisplayMenu():
+    print("Enter some numbers separated by commas (e.g. 5, 67,37)")
+
+def get_user_input():
+    nums= input("Numbers:")
+    str_nums= nums.split(",")
+
+    for i in range(len(str_nums)):
+        str_nums[i] = float(str_nums[i])
+    
+    return str_nums
+
+def min_max_temperature(temp):
+    return [int(min(temp)), int(max(temp))]
+
+def average_temperature(temp):
+    sum =0
+    for x in (temp):
+        sum += x
+    
+    return (sum/len(temp))
+
+
